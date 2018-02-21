707446862b4207cdb8fb9d175ddc021543ccd8b2
David Capella <D_Capella@yahoo.com>
2018-02-21 14:22:04 -0600

    finished practice test

---
+++ testing/math.py
@@ -0,0 +1,10 @@
+def sum_list(new_list):
+    """Returns the sum of all numbers in given list.
+
+    >>> sum_list([1, 2, 3])
+    6
+    >>> sum_list([3, 5, 9])
+    17
+    """
+    return sum(new_list)
+    # return None
